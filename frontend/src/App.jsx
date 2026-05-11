0f2d2c4b27bc6630e5b4f323f7a1c9092479df89
achintyajain1502 <achintyajain1502@gmail.com>
2026-05-11 11:26:16 +0530

    Switched backend to Render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
       const formData = new FormData();
       formData.append("file", file);
 
-      const res = await axios.post("https://studymate-production-897e.up.railway.app/upload", formData);
+      const res = await axios.post("https://studymate-backend-mu7z.onrender.com/upload", formData);
 
       console.log("UPLOAD SUCCESS:", res.data);
 
@@ -80,7 +80,7 @@ function App() {
       setStatus("Generating answer...");
       setAnswer("");
 
-      const res = await axios.post("https://studymate-production-897e.up.railway.app/ask", {
+      const res = await axios.post("https://studymate-backend-mu7z.onrender.com/ask", {
         question,
       });
 
